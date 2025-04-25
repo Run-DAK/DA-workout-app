24749b6db9a6f4c4887050f69628e68f2a21e641
Run-DAK <151647140+Run-DAK@users.noreply.github.com>
2025-04-24 20:58:52 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,21 @@ const Sequelize = require('sequelize');
 //imports the ORm and dotenv to the environment 
 require('dotenv').config();
 
-//initializes a sequelize instance with env variables for security
+//initializes a sequelize instance with env variables for security, uses postgreSQL for render
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect:'mysql',
-    port: process.env.DB_PORT
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
